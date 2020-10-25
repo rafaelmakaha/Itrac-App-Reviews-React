e14ef545bd67a354f6c83e0b5c92f4cefdca03d7
Rafael Makaha <rafael.makaha@gmail.com>
2020-10-25 19:23:57 -0300

    Update Home Screen
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,15 +1,77 @@
-import React from 'react';
-import { Container, Row, Title } from './style';
+import React, { useEffect, useState } from 'react';
+import { getClusters } from '../../services/Clusters';
+
+import { 
+  Container,
+  Button,
+  Title, 
+  List,
+  Content,
+  Text,
+} from './style';
+import LoadingFullScreen from '../../components/LoadingFullScreen';
 
 const Home = () => {
+  const [clusters,setClusters] = useState([]);
+  const [selectedCluster, setSelectedCluster] = useState({values: {}});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(()=>{
+    requestClusters();
+
+  },[]);
+
+  const requestClusters = (cluster = '') => {
+    setIsLoading(true);
+    getClusters(cluster)
+    .then(res => {
+      // Same as an IF statement
+      // const fnSetState = cluster ? setSelectedCluster : setClusters;
+      // fnSetState(res.data)
+      if (cluster){
+        setSelectedCluster({cluster, values: res.data});
+      }else{
+        setClusters(res.data);
+      }
+      setIsLoading(false);
+    })
+  }
+
   return (
-    <Container>
-      <Row>
-          <Title>
-            Hello World
-          </Title>
-      </Row>
-    </Container>
+    <>
+      <LoadingFullScreen status={isLoading} />
+      <Container>
+        <List>
+          {
+            clusters.map((cluster,index) => (
+              <Button 
+                key={index} 
+                onClick={() => requestClusters(String(index))}>
+                  Cluster {index}
+              </Button>
+            ))
+          }
+        </List>
+        <Content>
+          {
+            !!selectedCluster.cluster && <Title>Cluster {selectedCluster.cluster}</Title>
+          }
+          {
+            Object.entries(selectedCluster.values).map(([key, value],index) => {
+              console.log(key,value)
+              return (
+                <React.Fragment key={key}>
+                  <Text>Mensagem {index}</Text>
+                  <Text>{value.text}</Text>
+                  <Text>Score: {value.score}</Text>
+                  <br></br>
+                </React.Fragment>
+              )
+            })
+          }
+        </Content>
+      </Container>
+    </>
   );
 }
 
--- src/views/Home/style.js
+++ src/views/Home/style.js
@@ -3,27 +3,14 @@ import styled from 'styled-components';
 import { IMAGES } from '../../settings'
 
 export const Container = styled.div`
-  display   : flex;
-  height    : 100vh;
-`;
-
-export const Row = styled.div`
-  margin      : auto;
-  max-width   : 1280px;
-  width       : 75%;
-
-  &::before {
-    background-image      : url(${IMAGES.reactLogo});
-    background-position   : 30vw 30vh;
-    background-repeat     : no-repeat;
-    background-size       : 100vh auto;
-    content               : " ";
-    position              : absolute;
-    top                   : 0;
-    bottom                : 0;
-    left                  : 0;
-    right                 : 0;
-    opacity               : 0.1;
+  display               : grid;
+  grid-template-columns : 30% 1fr;
+  gap                   : 10px;
+  height                : 100vh;
+  max-width             : 100vw;
+  padding               : 10px;
+  @media (min-width: 640px) {
+    grid-template-columns : 200px 1fr;
   }
 `;
 
@@ -34,4 +21,22 @@ export const Title = styled.h1`
   font-weight     : bold;
   letter-spacing  : 0.1em;
   text-align      : center;
+`;
+
+export const Button = styled.button`
+
+`;
+
+export const List = styled.div`
+  display         : flex;
+  flex-direction  : column;
+  gap             : 3px;
+`;
+
+export const Content = styled.div`
+  
+`;
+
+export const Text = styled.p`
+
 `;
